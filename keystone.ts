927886e2dd9c16b025beb490698f2b544b70d20e
FTLIKON <854892057@qq.com>
2022-11-12 16:54:56 +0800

    update: db connect

--- keystone.ts
+++ keystone.ts
@@ -5,23 +5,20 @@
 // Keystone imports the default export of this file, expecting a Keystone configuration object
 //   you can find out more at https://keystonejs.com/docs/apis/config
 
-import { config } from '@keystone-6/core';
+import { config } from "@keystone-6/core";
 
 // to keep this file tidy, we define our schema in a different file
-import { lists } from './schema';
+import { lists } from "./schema";
 
 // authentication is configured separately here too, but you might move this elsewhere
 // when you write your list-level access control functions, as they typically rely on session data
-import { withAuth, session } from './auth';
+import { withAuth, session } from "./auth";
 
 export default withAuth(
   config({
     db: {
-      // we're using sqlite for the fastest startup experience
-      //   for more information on what database might be appropriate for you
-      //   see https://keystonejs.com/docs/guides/choosing-a-database#title
-      provider: 'sqlite',
-      url: 'file:./keystone.db',
+      provider: "postgresql",
+      url: "postgresql://postgres:nes816224@localhost:5432/eachstar_dev",
     },
     lists,
     session,
